8e5eab86abeffec3e5872ae8d99e1150845e42b7
yeshapearlsamera <yeshapearlsamera32@gmail.com>
2024-05-26 14:08:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ function countLike1() {
   let likeCounts = parseInt(count1.value) + 1
   count1.textContent = likeCounts.toString()
 }
-btnLike1.addEventListener("click", countLike1)
 
+btnLike1.addEventListener("click", countLike1)
 
 const btnLike2 = document.getElementById("btnLike2")
 const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count.value) + 1
+  let likeCounts = parseInt(count2.value) + 1
   count2.textContent = likeCounts.toString()
 }
 
 btnLike2.addEventListener("click", countLike2)
 
-const btnDisLike1 = document.getElementById("btnDisLike1")
+const btnDislike1 = document.getElementById("btnDislike1")
 const count3 = document.getElementById("count3")
 
-function countDisLike1() {
-  let DisLikeCounts = parseInt(count3.value) + 1
-  count3.textContent = likeCounts.toString()
+function countDislike1() {
+  let DislikeCounts = parseInt(count3.value) + 1
+  count3.textContent = DislikeCounts.toString()
 }
-btnDisLike1.addEventListener("click", countDisLike1)
 
+btnDislike1.addEventListener("click", countDislike1)
 
-
-
-const btnDisLike2 = document.getElementById("btnDisLike2")
+const btnDislike2 = document.getElementById("btnDislike2")
 const count4 = document.getElementById("count4")
+
+function countDislike2() {
+  let DislikeCounts = parseInt(count4.value) + 1
+  count4.textContent = DislikeCounts.toString()
 }
-function countLike2() {
-  let DisLikeCounts = parseInt(count4.value) + 1
-  count4.textContent = likeCounts.toString()
-}
-btnDisLike2.addEventListener("click", countDisLike2)
+
+btnDislike2.addEventListener("click", countDislike2)
