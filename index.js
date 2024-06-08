ed653fab4d16ee96d9d3b0aedd9a8f8d06ee5387
yeshapearlsamera <yeshapearlsamera32@gmail.com>
2024-06-08 09:43:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,13 @@ function countDislike2() {
 }
 
 btnDislike2.addEventListener("click", countDislike2)
+
+const submit  = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+
+function submitComment()}
+  commentbox.textContent += comment.value.toString() + "\n"
+}
+submit.addEventListener("click",submitComment)
+  
